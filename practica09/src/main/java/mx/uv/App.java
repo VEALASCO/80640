1664ffed72507ab649150a79ba95c0e259d04357
VELASCO <velascoale83@gmail.com>
2023-10-19 12:56:53 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,5 +1,7 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hola mundo" );
+
+        //utilizamos un método u objeto get y ponemos una función lambda(anónima).
+        get("/", 
+        (request,response)-> "<h1>Hola mundo!</h1>"
+        );
+        get("/ruta1", 
+        (request,response)-> "<h1>adios mundo!</h1>"
+        );
+        get("/ruta2", 
+        (request,response)-> "<h1>que tal mundo!</h1>"
+        );
+        get("/ruta3", 
+        (request,response)-> "<h1>ok mundo!</h1>"
+        );
     }
 }
